01049ee92042092fd86c24dd70ecf8aea2b8fcb0
Ehsan Amirahmadi <EhsanAmirahmadi1385@gmail.com>
2025-12-31 00:53:35 +0330

    Update game.py

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 import random
 javab = random.randint(1, 99)
 
-print('Hi. weloome to number game! I have a number. you must say a guess in range 1,99 .')
+print('Hi. welcome to number game! I have a number. you must guess a number in range 1,99 .')
 name = input('What is your name?: ')
 hads = int(input('What is your guess?: '))
 
